2079ecde963fab84a4f01d648b9011a373f17a1c
Brian Maher <maherb@brimworks.com>
2009-05-18 23:56:40 -0700

    Use make-common to build lua.

---
+++ module.mk
@@ -0,0 +1,51 @@
+pwd := $(pwd)
+
+include $(make-common.dir)/tool/cc.mk
+include $(make-common.dir)/tool/cp.mk
+include $(make-common.dir)/layout.mk
+
+######################################################################
+
+_lib  := $(lib.dir)/liblua.so
+_objs := $(call cc.c.to.o,$(addprefix $(pwd)/src/, \
+	lapi.c lcode.c ldebug.c ldo.c ldump.c lfunc.c lgc.c llex.c lmem.c \
+	lobject.c lopcodes.c lparser.c lstate.c lstring.c ltable.c ltm.c  \
+	lundump.c lvm.c lzio.c \
+	lauxlib.c lbaselib.c ldblib.c liolib.c lmathlib.c loslib.c ltablib.c \
+	lstrlib.c loadlib.c linit.c \
+))
+
+all: | $(_lib)
+$(_lib): $(_objs)
+	$(cc.so.rule)
+
+######################################################################
+
+_app  := $(bin.dir)/lua
+_objs := $(call cc.c.to.o,$(addprefix $(pwd)/src/, \
+	lua.c \
+))
+
+all: | $(_app)
+$(_app): cc.libs += lua
+$(_app): $(_objs)
+	$(cc.exe.rule)
+
+######################################################################
+
+_app  := $(bin.dir)/luac
+_objs := $(call cc.c.to.o,$(addprefix $(pwd)/src/, \
+	luac.c print.c \
+))
+
+all: | $(_app)
+$(_app): cc.libs += lua
+$(_app): $(_objs)
+	$(cc.exe.rule)
+
+######################################################################
+# Export headers:
+$(include.dir)/%.h: $(pwd)/src/%.h
+	$(cp.rule)
+
+all: $(addprefix $(include.dir)/,lua.h luaconf.h lualib.h lauxlib.h)
